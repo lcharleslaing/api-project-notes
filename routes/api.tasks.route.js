5200b178fdac00f62d63f2755d49d186256fae1d
leecharleslaing <71520336+lcharleslaing@users.noreply.github.com>
2022-04-06 19:30:42 -0400

    Initial commit

---
+++ routes/api.tasks.route.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient;
+
+const from = "tasks" // await prisma.'users' (SHOULD MATCH)
+const prismaObject = prisma.task
+
+// Get All
+router.get(`/${from}`, async (req, res, next) => {
+  try {
+    const records = await prismaObject.findMany({
+      include: {
+        projects: true
+      }
+    })
+    res.json(records)
+  } catch (error) {
+    next(error)
+  }
+});
+
+// GET by ID
+router.get(`/${from}/:id`, async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const record = await prismaObject.findUnique({
+      where: {
+        id: Number(id)
+      },
+    })
+    res.json(record)
+  } catch (error) {
+    next(error)
+  }
+});
+
+
+// POST NEW
+router.post(`/${from}`, async (req, res, next) => {
+  try {
+    const record = await prismaObject.create({
+      data: req.body
+    })
+    res.json(record)
+  } catch (error) {
+    next(error)
+  }
+});
+
+
+// DELETE by ID
+router.delete(`/${from}/:id`, async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const record = await prismaObject.delete({
+      where: {
+        id: Number(id)
+      },
+    })
+    res.json(record)
+  } catch (error) {
+    next(error)
+  }
+});
+
+
+// UPDATE by ID
+router.patch(`/${from}/:id`, async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const record = await prismaObject.update({
+      where: {
+        id: Number(id)
+      },
+      data: req.body
+    })
+    res.json(record)
+  } catch (error) {
+    next(error)
+  }
+});
+
+module.exports = router;
